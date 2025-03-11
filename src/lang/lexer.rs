2c666579e332ef06a45f153994e0f28552dc657e
Nixii <161391079+nixii@users.noreply.github.com>
2025-03-10 21:17:22 -0700

    Modified the lexer to use iterators instead of a loop

--- src/lang/lexer.rs
+++ src/lang/lexer.rs
@@ -1,6 +1,40 @@
 
+use std::collections::VecDeque;
+
 // All imported modules
 use super::token::Token;
+
+// Tokenize an identifier
+fn tokenize_identifier(chars: &mut VecDeque<char>, c: char) -> Token {
+    // Setup the data for the token
+    let mut token_data = String::from(c);
+
+    // Loop through characters until the identifier can't be continued
+    // Would be cleaner but you can't use a while let in a while let
+    // loop {
+    //     let c2 = chars.pop_front();
+    //     match c2 {
+    //         Some(x) => {
+    //             if x.is_alphanumeric() {
+    //                 token_data.push(x);
+    //             } else {
+    //                 break;
+    //             }
+    //         },
+    //         None => break
+    //     }
+    // }
+    let alphanumeric_chars: VecDeque<_> = chars.iter()
+        .take_while(|c| c.is_alphanumeric())
+        .cloned()
+        .collect();
+
+    chars.drain(0..alphanumeric_chars.len());
+    token_data.extend(alphanumeric_chars);
+
+    // Return the token
+    Token::Identifier(token_data)
+}
 
 // Tokenize a string
 pub fn tokenize(text: String) -> Vec<Token> {
@@ -9,25 +43,32 @@ pub fn tokenize(text: String) -> Vec<Token> {
     let mut tokens: Vec<Token> = Vec::new();
 
     // Get all the characters
-    let mut chars = text.chars();
+    let mut chars: VecDeque<char> = VecDeque::from_iter(text.chars());
 
     // For all the characters
-    while let Some(c) = chars.nth(0) {
+    while let Some(c) = chars.pop_front() {
 
         // If the character is in the alphabet
         if c.is_alphabetic() {
 
-            // Setup the data for the token
-            let mut token_data = String::from(c);
+            // Create the token and push it
+            let tok = tokenize_identifier(&mut chars, c);
+            tokens.push(tok);
 
-            // Loop through characters until the identifier can't be continued
-            // Would be cleaner but you can't use a while let in a while let
+        // If the character is a digit
+        } else if c.is_digit(10) {
+            let mut num_str = String::from(c);
+            let mut dot = false;
+            
             loop {
-                let c2 = chars.nth(0);
+                let c2 = chars.pop_front();
                 match c2 {
                     Some(x) => {
-                        if x.is_alphanumeric() {
-                            token_data.push(x);
+                        if x.is_digit(10) || x == '.' {
+                            if x == '.' {
+                                dot = true;
+                            }
+                            num_str.push(x);
                         } else {
                             break;
                         }
@@ -36,10 +77,9 @@ pub fn tokenize(text: String) -> Vec<Token> {
                 }
             }
 
-            // Push the identifier in
-            tokens.push(Token::Identifier(token_data));
-        } else if c.is_digit(10) {
-
+            if dot {
+                
+            }
         }
     }
 
--- src/lang/token.rs
+++ src/lang/token.rs
@@ -22,16 +22,7 @@ pub enum Token {
     Semicolon,
 
     // Number types (Whooo a lot)
-    Integer8(i8),
-    Integer16(i16),
-    Integer32(i32),
-    Integer64(i64),
-    Integer128(i128),
-    UInteger8(u8),
-    UInteger16(u16),
-    UInteger32(u32),
-    UInteger64(u64),
-    UInteger128(u128),
-    Float32(f32),
-    Float64(f64),
+    Integer(i32),
+    UInteger(u32),
+    Float(f32),
 }
