ea67fed270515ff2b6bc571868e70a236d0ad240
Nixii <161391079+nixii@users.noreply.github.com>
2025-03-10 20:52:43 -0700

    Keywords are tokenized

--- src/lang/lexer.rs
+++ src/lang/lexer.rs
@@ -6,7 +6,7 @@ use super::token::Token;
 pub fn tokenize(text: String) -> Vec<Token> {
 
     // Return tokens
-    let tokens: Vec<Token> = Vec::new();
+    let mut tokens: Vec<Token> = Vec::new();
 
     // Get all the characters
     let mut chars = text.chars();
@@ -14,8 +14,30 @@ pub fn tokenize(text: String) -> Vec<Token> {
     // For all the characters
     while let Some(c) = chars.nth(0) {
 
+        // If the character is in the alphabet
         if c.is_alphabetic() {
 
+            // Setup the data for the token
+            let mut token_data = String::from(c);
+
+            // Loop through characters until the identifier can't be continued
+            // Would be cleaner but you can't use a while let in a while let
+            loop {
+                let c2 = chars.nth(0);
+                match c2 {
+                    Some(x) => {
+                        if x.is_alphanumeric() {
+                            token_data.push(x);
+                        } else {
+                            break;
+                        }
+                    },
+                    None => break
+                }
+            }
+
+            // Push the identifier in
+            tokens.push(Token::Identifier(token_data));
         } else if c.is_digit(10) {
 
         }
